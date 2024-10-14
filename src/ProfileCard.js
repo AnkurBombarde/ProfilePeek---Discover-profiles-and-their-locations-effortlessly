bcac54f7bc15983babd3b4062c3aa76250c92e11
Ankur Bombarde <145347832+AnkurBombarde@users.noreply.github.com>
2024-10-14 08:13:29 +0530

    ProfileCard.js

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,26 +1,3 @@
-/* import React from "react";
-import { useNavigate } from "react-router-dom";
-
-function Profile({ name, description, imageUrl, profile, onSummaryClick }) {
-  const navigate=useNavigate();
-
-  const handleSummaruCLick=()=>{
-    navigate('/summary', {state:{profile}});
-  };
-  
-  return (
-    <div className="profile-card">
-      <img src={imageUrl} alt="Profile" className="profile-image" />
-      <div className="profile-details">
-        <button className="summary-button" onClick={()=>onSummaryClick(profile)}>Summary</button>
-        <h3 className="profile-name">{name}</h3>
-        <p className="profile-description">{description}</p>
-      </div>
-    </div>
-  );
-}
-
-export default Profile; */
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
