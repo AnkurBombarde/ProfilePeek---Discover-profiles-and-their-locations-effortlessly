2ae3d93106c09b4c485e680475e8e15fefdafd76
Ankur Bombarde <145347832+AnkurBombarde@users.noreply.github.com>
2024-10-14 08:14:44 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,3 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
